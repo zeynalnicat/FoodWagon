df65cf81975934067724100b30b8752ef11b0df5
Nicat Zeynalli <nicat21308@gmail.com>
2023-09-16 18:42:10 +0400

    link login navigation in router

--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
+import Login from "../pages/Login";
 
 
 const WebRouting = () => {
@@ -8,6 +9,7 @@ const WebRouting = () => {
     <Routes>
       <Route path="/" element={<Home/>}  />
       <Route path="/cart" element={<Cart/>}  />
+      <Route path="/login" element={<Login/>}  />
     </Routes>
   );
 };
