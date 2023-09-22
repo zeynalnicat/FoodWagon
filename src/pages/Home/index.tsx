329f2fa2de682bb941dbf2edae2f0c5cb15027bb
Nicat Zeynalli <nicat21308@gmail.com>
2023-09-22 19:37:57 +0400

    adding two new components to home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,30 @@
-import { Stack } from '@mui/material'
-import Header from '../../components/Header'
-import Banner from '../../components/Banner'
-import FoodDiscount from '../../components/FoodDiscount'
-import HowWork from '../../components/HowWork'
-import PopularItems from '../../components/PopularItems'
-import Footer from '../../components/Footer'
+import { Stack } from "@mui/material";
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import FoodDiscount from "../../components/FoodDiscount";
+import HowWork from "../../components/HowWork";
+import PopularItems from "../../components/PopularItems";
+import Footer from "../../components/Footer";
+import FeaturedRestaurants from "../../components/FeaturedRestaurants";
+import FooterUp from "../../components/FooterUp";
 
 const Home = () => {
   return (
     <Stack>
-        <Header/>
-        <Banner title='Are you starving?' subtitle='Within a few clicks, find meals that are accessible near you' isHomePage/>
-        <FoodDiscount/>
-        <HowWork/>
-        <PopularItems/>
-          <Footer/>
-      </Stack>
-  )
-}
+      <Header />
+      <Banner
+        title="Are you starving?"
+        subtitle="Within a few clicks, find meals that are accessible near you"
+        isHomePage
+      />
+      <FoodDiscount />
+      <HowWork />
+      <PopularItems />
+      <FeaturedRestaurants />
+      <FooterUp />
+      <Footer />
+    </Stack>
+  );
+};
 
-export default Home
+export default Home;
